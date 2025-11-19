6a19b670d9306b613185522caa07b13dbbbb387c
ibnuahmadfauzi <ibnu7897@gmail.com>
2025-11-20 00:10:27 +0700

    modification portofolio kegiatan diklat

--- resources/views/client-page/pages/bidang-kerja/diklat/html.blade.php
+++ resources/views/client-page/pages/bidang-kerja/diklat/html.blade.php
@@ -18,11 +18,42 @@
                         <div class="col-lg-8">
                             <div id="bidangkerja-konsultasipendidikan-container">
                                 <div id="canvas-portofoliokegiatan">
-                                    <h3><i class="fa-solid fa-crown"></i> Portofolio Kegiatan</h3>
+                                    <h3 class="mb-4"><i class="fa-solid fa-crown"></i> Portofolio Kegiatan</h3>
                                     <div class="content">
-                                        <p class="text-center py-5">
-                                            belum ada informasi yang ditampilkan
-                                        </p>
+                                        <div id="carouselExampleIndicators" class="carousel slide">
+                                            <div class="carousel-indicators">
+                                                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+                                                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                                                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+                                                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="3" aria-label="Slide 4"></button>
+                                                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="4" aria-label="Slide 5"></button>
+                                            </div>
+                                            <div class="carousel-inner">
+                                                <div class="carousel-item active">
+                                                <img src="/assets/images/bidangkerja/diklat/galeri/diklat-1.png" class="d-block w-100" alt="Yayasan Pengembangan Mutu Pendidikan - Jawa Timur">
+                                                </div>
+                                                <div class="carousel-item">
+                                                <img src="/assets/images/bidangkerja/diklat/galeri/diklat-2.png" class="d-block w-100" alt="Yayasan Pengembangan Mutu Pendidikan - Jawa Timur">
+                                                </div>
+                                                <div class="carousel-item">
+                                                <img src="/assets/images/bidangkerja/diklat/galeri/diklat-3.png" class="d-block w-100" alt="Yayasan Pengembangan Mutu Pendidikan - Jawa Timur">
+                                                </div>
+                                                <div class="carousel-item">
+                                                <img src="/assets/images/bidangkerja/diklat/galeri/diklat-4.png" class="d-block w-100" alt="Yayasan Pengembangan Mutu Pendidikan - Jawa Timur">
+                                                </div>
+                                                <div class="carousel-item">
+                                                <img src="/assets/images/bidangkerja/diklat/galeri/diklat-5.png" class="d-block w-100" alt="Yayasan Pengembangan Mutu Pendidikan - Jawa Timur">
+                                                </div>
+                                            </div>
+                                            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+                                                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                                                <span class="visually-hidden">Previous</span>
+                                            </button>
+                                            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+                                                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                                                <span class="visually-hidden">Next</span>
+                                            </button>
+                                        </div>
                                     </div>
                                 </div>
                                 <div id="canvas-tekniskerjasama">
