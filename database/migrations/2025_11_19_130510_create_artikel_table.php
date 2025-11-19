c37ee0c0ec7fad3d32790a174bf61f2ce3523055
ibnuahmadfauzi <ibnu7897@gmail.com>
2025-11-19 21:11:55 +0700

    add artikel seeder

---
+++ database/migrations/2025_11_19_130510_create_artikel_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateArtikelTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('artikel', function (Blueprint $table) {
+            $table->id();
+            $table->string('judul');
+            $table->string('slug');
+            $table->longText('body');
+            $table->string('categories');
+            $table->string('thumbnail');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('artikel');
+    }
+}
