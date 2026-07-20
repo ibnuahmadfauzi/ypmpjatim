de49f8a0a71511747029a2a81922246d62c72dbf
ibnuahmadfauzi <ibnu7897@gmail.com>
2026-07-21 06:07:35 +0700

    homepage create tentang kami section

--- resources/views/frontend/pages/home/html.blade.php
+++ resources/views/frontend/pages/home/html.blade.php
@@ -20,3 +20,30 @@
         <img src="/assets/images/banner.png" alt="Yayasan Pengembangan Mutu Pendidikan - Jawa Timur">
     </div>
 </section>
+
+<section class="homepage-tentang container-fluid">
+    <div class="container">
+        <h2>~ Tentang Kami ~</h2>
+        <div class="row align-items-center">
+            <div class="col-lg-6">
+                <div class="d-flex justify-content-center">
+                    <img src="/assets/images/logo-ypmp.png" alt="Yayasan Pengembangan Mutu Pendidikan - Jawa Timur">
+                </div>
+            </div>
+            <div class="col-lg-6">
+                <p>
+                    <strong>Yayasan Pengembangan Mutu Pendidikan - Jawa Timur (YPMP JATIM)</strong> adalah perusahaan
+                    yang berdedikasi
+                    untuk memajukan dunia pendidikan melalui penerbitan buku dan jurnal berkualitas serta berbagai
+                    program pelatihan, penelitian, dan akreditasi. Berdiri dengan komitmen untuk menyediakan sumber daya
+                    edukatif yang handal dan terpercaya, kami terus berupaya memberikan kontribusi nyata bagi kemajuan
+                    pendidikan di Indonesia.
+                </p>
+                <div>
+                    <a href="#" class="btn btn-sm btn-primary"><i
+                            class="fa-solid fa-arrow-up-right-from-square"></i> Selengkapnya</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
