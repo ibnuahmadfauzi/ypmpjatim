1e89f8e4d14e4cccaa651955e926a775557536fa
ibnuahmadfauzi <ibnu7897@gmail.com>
2026-07-21 08:36:30 +0700

    finish create footer and homepage

---
+++ resources/views/frontend/partials/footer.blade.php
@@ -0,0 +1,84 @@
+<section class="partner-section container-fluid">
+    <div class="container">
+        <h2>~ Partner Kami ~</h2>
+        <div class="partner-list">
+            <img src="/assets/images/partner/mncpublishing.jpg" alt="Partner 1">
+            <img src="/assets/images/partner/bcn.jpg" alt="Partner 2">
+            <img src="/assets/images/partner/praisebooks.jpg" alt="Partner 3">
+            <img src="/assets/images/partner/msa.jpg" alt="Partner 4">
+            <img src="/assets/images/partner/ezekielpublishing.jpg" alt="Partner 5">
+        </div>
+    </div>
+</section>
+
+<footer class="container-fluid">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-6">
+                <h2>YPMP JATIM</h2>
+                <div class="footer-content">
+                    <p>
+                        <strong>Alamat:</strong>
+                        <br>
+                        Jl. KH. Abdul Fatah, No. 31, RT 003, RW 004, Jatinom, Kanigoro, kab. Blitar, 66171
+                    </p>
+                    <p>
+                        <strong>No. Telp:</strong>
+                        <br>
+                        0857-5555-0745
+                        <br>
+                        0813-5808-5388
+                    </p>
+                    <p>
+                        <strong>
+                            Email:
+                        </strong>
+                        <br>
+                        ypmpjatim@gmail.com
+                    </p>
+                </div>
+                <br><br>
+            </div>
+            <div class="col-lg-6">
+                <h2>KIRIM PESAN</h2>
+                <div class="footer-content">
+                    <form action="">
+                        <div class="mb-3">
+                            <div class="row">
+                                <div class="col-lg-6">
+                                    <label for="pesan-nama" class="form-label">Nama:</label>
+                                    <input type="text" class="form-control" id="pesan-nama" placeholder="...">
+                                </div>
+                                <div class="col-lg-6">
+                                    <label for="pesan-email" class="form-label">Email:</label>
+                                    <input type="email" class="form-control" id="pesan-email" placeholder="...">
+                                </div>
+                            </div>
+                        </div>
+                        <div class="mb-3">
+                            <label for="pesan-subjek" class="form-label">Subjek:</label>
+                            <input type="text" class="form-control" id="pesan-subjek" placeholder="...">
+                        </div>
+                        <div class="mb-3">
+                            <label for="pesan-pesan" class="form-label">Pesan:</label>
+                            <textarea id="pesan-pesan" class="form-control" rows="5"></textarea>
+                        </div>
+                        <div>
+                            <button type="submit" class="btn btn-sm btn-light"><i class="fa-solid fa-paper-plane"></i>
+                                Kirim Pesan</button>
+                            <button type="reset" class="btn btn-sm btn-secondary"><i class="fa-solid fa-trash"></i>
+                                Batal</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
+
+<section class="copyright-section container-fluid">
+    <div class="container">
+        <p>Copyright &copy; 2026 <strong>
+                Yayasan Pengembangan Mutu Pendidikan</strong>. All Right Reserved.</p>
+    </div>
+</section>
